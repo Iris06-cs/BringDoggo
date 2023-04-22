6e97d274bc3b3da6e9611fef5afd19f5276453e1
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-04-22 10:28:45 -0700

    added review model

---
+++ backend/app/models/review.py
@@ -0,0 +1,46 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.schema import CheckConstraint
+from datetime import datetime
+
+class Review(db.Model):
+    """
+    Review model representing reviews in the application.
+    """
+    __tablename__="reviews"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id=db.Column(db.Integer,primary_key=True)
+    stars=db.Column(db.Integer,nullable=False)
+    review=db.Column(db.String(1000),nullable=False)
+    created_at = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow()
+    )
+    updated_at = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow(), onupdate=datetime.utcnow
+    )
+    author_id=db.Column(db.Integer,db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    restaurant_id=db.Column(db.String,db.ForeignKey(add_prefix_for_prod("restaurants.id")),nullable=False)
+
+    __table_args__ = (
+        CheckConstraint("stars >= 1 AND stars <= 5", name="check_stars_range"),
+    )
+    # relationship
+    users=db.relationship("User",back_populates="user_reviews")
+
+    reviewed_restaurants=db.relationship("Restaurant",back_populates="restaurant_reviews")
+
+    def to_dict(self):
+        """
+        Convert the Review object to a dictionary representation.
+        """
+        return{
+            'id':self.id,
+            'stars':self.stars,
+            'review':self.review,
+            'restaurantId':self.restaurant_id,
+            'authorId':self.author_id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+        }
