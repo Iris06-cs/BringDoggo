229dfb4033dde97f6d9a069a32f16dc4ca0963d7
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-04-29 02:45:45 -0700

    restaurants store, component

--- frontend/src/components/AllRestaurantsPage/index.js
+++ frontend/src/components/AllRestaurantsPage/index.js
@@ -1,5 +1,28 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllRestaurants } from "../../store/restaurants";
+
+import FilterTabs from "./FilterTabs";
+import Map from "./Map";
+import RestaurantCard from "./RestaurantCard";
+
 const AllRestaurantsPage = () => {
-  return <h1>working</h1>;
+  const dispatch = useDispatch();
+  const allRestaurants = useSelector((state) => state.restaurants.restaurants);
+
+  useEffect(() => {
+    dispatch(getAllRestaurants());
+  }, [dispatch]);
+  if (!allRestaurants) return <h1>Loading...</h1>;
+  return (
+    <div>
+      <FilterTabs />
+      {Object.values(allRestaurants).map((restaurant, idx) => (
+        <RestaurantCard key={idx} restaurant={restaurant} idx={idx + 1} />
+      ))}
+      <Map />
+    </div>
+  );
 };
 
 export default AllRestaurantsPage;
