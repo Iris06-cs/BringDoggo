27f2c7d83719a076ed206c21b908b28a6384983e
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-04-29 13:35:14 -0700

    added pagination

--- frontend/src/components/AllRestaurantsPage/index.js
+++ frontend/src/components/AllRestaurantsPage/index.js
@@ -1,25 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllRestaurants } from "../../store/restaurants";
 
 import FilterTabs from "./FilterTabs";
 import Map from "./Map";
 import RestaurantCard from "./RestaurantCard";
+import PageNumbers from "./PageNumbers";
 
 const AllRestaurantsPage = () => {
   const dispatch = useDispatch();
   const allRestaurants = useSelector((state) => state.restaurants.restaurants);
+  const loadingRestaurants = useSelector(
+    (state) => state.restaurants.isLoading
+  );
+  const totalResults = useSelector(
+    (state) => state.restaurants.totalRestaurants
+  );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [startNum, setStartNum] = useState(1);
+  const pageNumber = Math.ceil(totalResults / 20); //max 480/20=24
 
   useEffect(() => {
-    dispatch(getAllRestaurants());
+    dispatch(getAllRestaurants(1));
   }, [dispatch]);
-  if (!allRestaurants) return <h1>Loading...</h1>;
+  console.log(currentPage);
+  const handlePageChange = (pageNum) => {
+    setCurrentPage(pageNum);
+    dispatch(getAllRestaurants(pageNum));
+  };
+  const displayRestaurantIdx = (idx) => {
+    return idx + 1 + (currentPage - 1) * 20;
+  };
+  if (!allRestaurants || loadingRestaurants) return <h1>Loading...</h1>;
+
   return (
     <div>
       <FilterTabs />
       {Object.values(allRestaurants).map((restaurant, idx) => (
-        <RestaurantCard key={idx} restaurant={restaurant} idx={idx + 1} />
+        <RestaurantCard
+          key={idx}
+          restaurant={restaurant}
+          idx={displayRestaurantIdx(idx)}
+        />
       ))}
+      <PageNumbers
+        pageNumber={pageNumber}
+        onPageChange={handlePageChange}
+        currentPage={currentPage}
+        startNum={startNum}
+        setStartNum={setStartNum}
+      />
       <Map />
     </div>
   );
--- frontend/src/store/restaurants.js
+++ frontend/src/store/restaurants.js
@@ -5,31 +5,40 @@ export const restaurantsSlice = createSlice({
   initialState: {
     restaurants: {},
     error: null,
+    isLoading: false,
+    totalRestaurants: null,
   },
   extraReducers: (builder) => {
     builder
       .addCase(getAllRestaurants.fulfilled, (state, action) => {
-        state.restaurants = action.payload;
+        // restaurants array
+        let res = {};
+
+        action.payload.restaurants.forEach((restaurant) => {
+          res[restaurant.id] = restaurant;
+        });
+        state.restaurants = res;
+        state.isLoading = false;
+        state.totalRestaurants = action.payload.totalResults;
+      })
+      .addCase(getAllRestaurants.pending, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = true;
       })
       .addCase(getAllRestaurants.rejected, (state, action) => {
         state.error = action.payload;
+        state.isLoading = false;
       });
   },
 });
 export const getAllRestaurants = createAsyncThunk(
   "restaurants/getAllRestaurants",
-  async (_, { rejectWithValue }) => {
-    const response = await fetch("/api/restaurants/");
+  async (page = 1, { rejectWithValue }) => {
+    const response = await fetch(`/api/restaurants/?page=${page}`);
     const data = await response.json();
 
     if (!response.ok) return rejectWithValue(data);
-    // restaurants array
-    let res = {};
-
-    data.restaurants.forEach((restaurant) => {
-      res[restaurant.id] = restaurant;
-    });
-    return res;
+    return data;
   }
 );
 
