6896f96f336815c94c4a5dd647539d4b71ab7978
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-05-08 18:02:33 -0700

    added update/delete button

--- frontend/src/components/RestaurantDetailPage/Reviews/index.js
+++ frontend/src/components/RestaurantDetailPage/Reviews/index.js
@@ -1,22 +1,70 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useRef, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import dateFormater from "../../../utils/dateFormater";
 import Ratings from "../../AllRestaurantsPage/Ratings/Ratings";
 import "./Reviews.css";
 import placeHolderImg from "../../../image/user-icon.png";
 
 import RatingTooltip from "./RatingTooltip";
-
-const Reviews = ({ restaurantDetail }) => {
+import { deleteReview } from "../../../store/reviews";
+import { getRestaurantById } from "../../../store/restaurants";
+const Reviews = ({ restaurantDetail, setHasReview }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
   const currentUser = useSelector((state) => state.session.user);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef();
+  const restaurantReviews = restaurantDetail.reviews;
+  useEffect(() => {
+    if (!showDropdown) return;
 
-  const reviews = restaurantDetail.reviews;
+    const closeMenu = (e) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setShowDropdown(false);
+      }
+    };
 
-  if (!reviews) return <p>loading...</p>;
-  // no login user==>render write review component
-  //has login user==> user has exisiting review==>top review with edit delete button
-  //has login user==>user has no exisiting review==> render add review component
+    document.addEventListener("click", closeMenu);
 
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showDropdown]);
+
+  if (!restaurantReviews) return <p>loading...</p>;
+  const currUserReview = restaurantReviews.find(
+    (review) => currentUser && review.authorId === currentUser.id
+  );
+  const otherReviews = restaurantReviews.filter(
+    (review) => !currentUser || review.authorId !== currentUser.id
+  );
+  const reviews = currUserReview
+    ? [currUserReview, ...otherReviews]
+    : otherReviews;
+  const dropdownCls =
+    "modify-review-dropdown" + (showDropdown ? "" : " hidden");
+  const closeDropdown = () => setShowDropdown(false);
+  const openDropdown = () => {
+    if (showDropdown) setShowDropdown(false);
+    else setShowDropdown(true);
+  };
+
+  const handleDelete = async (reviewId) => {
+    closeDropdown();
+    setHasReview(false);
+    await dispatch(deleteReview(reviewId));
+    // fetch updated restaurant
+    await dispatch(getRestaurantById(restaurantDetail.id));
+  };
+  const handleUpdate = async () => {
+    const restaurantId = restaurantDetail.id;
+    const name = restaurantDetail.name;
+    const path = `/restaurants/${restaurantId}/reviews/new`;
+    const param = currUserReview
+      ? `?name=${name}&rating=${currUserReview.stars}`
+      : `?name=${name}`;
+
+    history.push(path + param);
+  };
   return (
     <div className="reviews-section-container">
       {!currentUser && (
@@ -66,10 +114,20 @@ const Reviews = ({ restaurantDetail }) => {
             </div>
             <div className="review-rating-container">
               <Ratings avgRating={review.stars} />
-              {/* edit/delete funtionality */}
-              <button className="edit-review-btn">
-                &middot;&middot;&middot;
-              </button>
+              {/* edit/delete funtionality for current user */}
+              {currentUser && currentUser.id === review.authorId && (
+                <>
+                  <button className="edit-review-btn" onClick={openDropdown}>
+                    &middot;&middot;&middot;
+                  </button>
+                  <ul className={dropdownCls} ref={dropdownRef}>
+                    <li onClick={() => handleUpdate()}>Update Review</li>
+                    <li onClick={() => handleDelete(review.id)}>
+                      Delete Review
+                    </li>
+                  </ul>
+                </>
+              )}
             </div>
           </div>
         ))}
