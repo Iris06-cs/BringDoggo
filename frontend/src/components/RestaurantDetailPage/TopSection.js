25b09b42fde093cd5ad1a5bb927382e2fda365fd
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-05-09 17:35:40 -0700

    added open time

--- frontend/src/components/RestaurantDetailPage/TopSection.js
+++ frontend/src/components/RestaurantDetailPage/TopSection.js
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
 import haruMenu from "../../image/haru-menu.jpg";
+import convertTimeFormat from "../../utils/convertTimeFormat";
 import Ratings from "../AllRestaurantsPage/Ratings/Ratings";
 
 const TopSection = ({ restaurantDetail }) => {
   const { name, avgRating, categories, dogReviewCount, price, hours } =
     restaurantDetail;
+  const [openTime, setOpenTime] = useState();
+  const [isOpen, setIsOpen] = useState();
+  const currDay = new Date();
+  const today = currDay.getDay();
+  const currHour = currDay.getHours();
+  const currMinut = currDay.getMinutes();
+  const curr24hTime = currHour * 100 + currMinut;
+  const todayHours = hours.length > 0 ? hours[0].open[today - 1] : undefined; //object
+  useEffect(() => {
+    if (todayHours)
+      setOpenTime(
+        `${convertTimeFormat(todayHours.start)}-${convertTimeFormat(
+          todayHours.end
+        )}`
+      );
+  }, [todayHours]);
+  useEffect(() => {
+    const checkIsOpen = () => {
+      const startTime = parseInt(todayHours.start, 10);
+      const endTime = parseInt(todayHours.end, 10);
+      setIsOpen(curr24hTime >= startTime && curr24hTime <= endTime);
+    };
+    checkIsOpen();
+    const timer = setInterval(() => {
+      checkIsOpen();
+    }, 60000); //check every min
+    return () => clearInterval(timer); //clean up
+  }, [curr24hTime, todayHours]);
   return (
     <div className="page-top-section">
       <div>
@@ -34,7 +64,12 @@ const TopSection = ({ restaurantDetail }) => {
             ))}
         </div>
         {/* need to show open/closed + hours, if open show current day open time, if closed show next day open time, as a link to click to the hours section */}
-        <p>hours placeholder</p>
+        {openTime && (
+          <p>
+            <span id="open-tag">{isOpen ? "Open" : "Closed"}</span>
+            {openTime}
+          </p>
+        )}
       </div>
     </div>
   );
