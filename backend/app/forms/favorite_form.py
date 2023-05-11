f1729c008537c2e96c8f3ea1fcc9a00ee2794fcd
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-05-11 15:18:53 -0700

    fix boolean field issue

--- backend/app/forms/favorite_form.py
+++ backend/app/forms/favorite_form.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class FavoriteForm(FlaskForm):
-    is_public=BooleanField('is_public',validators=[DataRequired()])
+    is_public=BooleanField('is_public')
     title=StringField('title',validators=[DataRequired()])
     description=StringField('description',validators=[DataRequired()])
