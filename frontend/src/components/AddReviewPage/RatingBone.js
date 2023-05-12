086688801b624fd7bf9e8816fe33eeabd19184f5
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-05-12 12:27:26 -0700

    fix edit review bug

--- frontend/src/components/AddReviewPage/RatingBone.js
+++ frontend/src/components/AddReviewPage/RatingBone.js
@@ -1,15 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
-const RatingBone = ({ selectedRating, setSelectedRating }) => {
+const RatingBone = ({
+  selectedRating,
+  setSelectedRating,
+  currRating,
+  setCurrRating,
+}) => {
   const [hoverRating, setHoverRating] = useState(0);
-  const onClickIcon = (bones) => setSelectedRating(bones);
+  const onClickIcon = (bones) => {
+    setSelectedRating(bones);
+    setCurrRating(bones);
+  };
   const onMouseIn = (bones) => setHoverRating(bones);
 
   const onMouseOut = () => setHoverRating(0);
 
   const toolkitText = ["Great", "Good", "Ok", "Not Good", "Bad"];
+  console.log(currRating);
 
+  const displayBones = (hoverRating, currRating, selectedRating) => {
+    let res;
+    if (hoverRating > 0) res = hoverRating;
+    else if (currRating) res = currRating;
+    else res = selectedRating;
+    return res;
+  };
   return (
     <>
       {/* hover over to show rating ratio,onclick=>add review page/login */}
@@ -17,7 +33,7 @@ const RatingBone = ({ selectedRating, setSelectedRating }) => {
         {toolkitText.map((text, idx) => (
           <div
             className={`single-bone-container ${
-              (hoverRating > 0 ? hoverRating : selectedRating) >= 5 - idx
+              displayBones(hoverRating, currRating, selectedRating) >= 5 - idx
                 ? "selected"
                 : ""
             }`}
