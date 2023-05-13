fb2b8ea9119686620f696b765a4a54d031a74e90
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-05-13 11:55:53 -0700

    add restaurant info to userProfile links

--- frontend/src/components/UserProfilePage/ProfileOverview.js
+++ frontend/src/components/UserProfilePage/ProfileOverview.js
@@ -1,4 +1,99 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllReviews,
+  getCurrentUserReviews,
+  selectCurrentUserReviews,
+} from "../../store/reviews";
+import {
+  getAllFavs,
+  getCurrentUserFavs,
+  selectCurrUserFavs,
+} from "../../store/favorites";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "../LoadingSpinner";
+import { getAllRestaurants } from "../../store/restaurants";
+import Ratings from "../AllRestaurantsPage/Ratings/Ratings";
+import { NavLink } from "react-router-dom";
+
 const ProfileOverview = () => {
-  return <h2>Latest Activity </h2>;
+  const dispatch = useDispatch();
+  const {
+    userReviews,
+    userFavs,
+    loader,
+    userFavIds,
+    userReviewIds,
+    allRestaurants,
+  } = useSelector((state) => ({
+    userReviews: selectCurrentUserReviews(state),
+    userFavs: selectCurrUserFavs(state),
+    userFavIds: state.favorites.currentUserFavoritesId,
+    userReviewIds: state.reviews.currentUserReviewIds,
+    loader: state.loader.loading,
+    allRestaurants: state.restaurants.restaurants,
+  }));
+  const [lastReview, setLastReview] = useState();
+  const [lastFav, setLastFav] = useState();
+  useEffect(() => {
+    dispatch(getAllRestaurants());
+    dispatch(getAllFavs());
+    dispatch(getAllReviews());
+    dispatch(getCurrentUserReviews());
+    dispatch(getCurrentUserFavs());
+  }, [dispatch]);
+  useEffect(() => {
+    if (allRestaurants && userFavs)
+      setLastFav(userFavs[userFavIds[userFavIds.length - 1]]);
+    if (allRestaurants && userReviews)
+      setLastReview(userReviews[userReviewIds[userReviewIds.length - 1]]);
+  }, [userFavs, userFavIds, userReviewIds, userReviews, allRestaurants]);
+  if (
+    loader ||
+    !userReviews ||
+    !userFavs ||
+    Object.values(allRestaurants).length < 1
+  )
+    return <LoadingSpinner />;
+  console.log(userFavs, userReviews);
+  return (
+    <>
+      <h3>Latest Activity </h3>
+      {!lastFav && !lastReview && <p>No recent activities.</p>}
+      {Object.values(allRestaurants).length > 0 && lastReview && (
+        <>
+          <p>Recent Review</p>
+          <div className="fav-card-left-container">
+            <div className="fav-title-row">
+              <NavLink
+                className="restaurant-name-link"
+                to={`/restaurants/${lastReview.restaurantId}`}
+              >
+                {" "}
+                {allRestaurants[lastReview.restaurantId].name}
+              </NavLink>
+            </div>
+            <div className="restaurant-review-info">
+              <div>{lastReview.reviewDetail}</div>
+              <Ratings avgRating={lastReview.stars} />
+            </div>
+          </div>
+        </>
+      )}
+
+      {Object.values(allRestaurants).length > 0 && lastFav && (
+        <>
+          <p>Recent Favorite Collection</p>
+          <div className="fav-card-left-container">
+            <div className="fav-title-row">{lastFav.title}</div>
+            <div>{lastFav.description}</div>
+            <div className="fav-collection-info">
+              <div>{lastFav.isPublic ? "Public" : "Non-Public"}</div>
+              <div>{Object.values(lastFav.restaurants).length}</div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 export default ProfileOverview;
--- frontend/src/components/UserProfilePage/UserReviews.js
+++ frontend/src/components/UserProfilePage/UserReviews.js
@@ -8,13 +8,14 @@ import {
 } from "../../store/reviews";
 import LoadingSpinner from "../LoadingSpinner";
 import { getAllRestaurants, getRestaurantById } from "../../store/restaurants";
+import { NavLink } from "react-router-dom";
 const UserReviews = ({ setIsDeleted }) => {
   const dispatch = useDispatch();
   const allRestaurants = useSelector((state) => state.restaurants.restaurants);
   const currUser = useSelector((state) => state.session.user);
   const userReviews = useSelector((state) => selectCurrentUserReviews(state));
   useEffect(() => {
-    // dispatch(getAllRestaurants()); //not getting all restaurant
+    dispatch(getAllRestaurants());
     dispatch(getAllReviews());
     dispatch(getCurrentUserReviews());
   }, [dispatch]);
@@ -24,16 +25,25 @@ const UserReviews = ({ setIsDeleted }) => {
     setIsDeleted((prev) => prev + 1);
   };
 
-  if (!userReviews) return <LoadingSpinner />;
+  if (!userReviews || Object.values(allRestaurants).length < 1)
+    return <LoadingSpinner />;
   return (
     <>
       <h3>Reviews</h3>
       <div className="userprofile-fav-cards-container">
         {userReviews &&
+          Object.values(allRestaurants).length > 0 &&
           Object.values(userReviews).map((review, idx) => (
             <div key={idx} className="fav-card-container">
               <div className="fav-card-left-container">
-                <div>Restaurant Info</div>
+                <div>
+                  <NavLink
+                    className="restaurant-name-link"
+                    to={`/restaurants/${review.restaurantId}`}
+                  >
+                    {allRestaurants[review.restaurantId].name}
+                  </NavLink>
+                </div>
                 <div>{review.reviewDetail}</div>
               </div>
               <div className="modify-fav-btns-container">
